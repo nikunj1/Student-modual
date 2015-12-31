1e1b85a48cc3c6635f6e4c775f6879a5ee648b5c
nikunj <ramoliya2013@gmail.com>
2015-12-31 10:42:42 +0530

    printing To_date to from_date has been complete

--- report/qweb.py
+++ report/qweb.py
@@ -8,10 +8,24 @@ class qweb_report(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(qweb_report, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
-            'get_index':self.get_index
+            'get_index':self.get_index,
+            'get_data' :self.get_data
                                 })
-   
-   
+    def get_data(self,data):
+        report_obj = self.pool.get('custom.report')
+      
+        catch = report_obj.browse(self.cr, self.uid, self.ids)
+        print catch.from_date
+        print catch.to_date
+        sale_order_ids = self.pool.get('sale.order').search(self.cr,self.uid,
+            [('create_date', '<=', catch.to_date),('create_date', '>=', catch.from_date)])
+    
+        print sale_order_ids
+        sheet_browse = self.pool.get('sale.order').browse(self.cr, self.uid, sale_order_ids)
+        print 'sheet'
+        print sheet_browse
+        return  sheet_browse
+        
     def get_index(self,index):
         print "\n\nproduct-----------------------"
         global product
