2089af10711b226aa47e997c224fe11b97332212
nikunj <ramoliya2013@gmail.com>
2015-12-31 10:43:27 +0530

    printing To_date to from_date has been complete

--- wizard/report_wizard.py
+++ wizard/report_wizard.py
@@ -1,20 +1,24 @@
 from openerp.osv import osv,fields
-
+import time
 
 class test_wizard(osv.osv_memory):
     _name = "custom.report"
     
     _columns = {
-                'from_date' : fields.date('From Date', required=True),
-                '' : fields.date('To Date', required=True),
+                'from_date' : fields.datetime('From Date', required=True),
+                'to_date' : fields.datetime('To Date', required=True),
             
                 
                 } 
     def report_print(self,cr,uid,id,context=None):
+        #sheet_pool = self.pool.get('res.student')
+
+        
         stage = self.browse(cr,uid,id)
         list = {}
-        list = {'to':stage.old,'from':stage.new}
-        print list
+        list = {'from_date':stage.from_date,'to_date':stage.to_date}
+        
+        
         
         return self.pool['report'].get_action(cr, uid, id, 'student.qweb_report_demo', data=list)
         
